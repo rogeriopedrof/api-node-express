db0bcfbfb4d9d6c646ad4afedf520d343ee6ef0c
rogeriopedrof <rogeriopedro.aa@gmail.com>
2025-11-25 17:59:21 -0300

    Routes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import publicRoutes from './routes/public.js';
+import router from './routes/public.js';
 
+// Initialize express app
 const app = express();
 const port = 3000;
 
+// Rota raiz
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
 
+// Middlewares
+app.use(express.json());
+app.use('/usuarios', publicRoutes);
+
+// Start server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
